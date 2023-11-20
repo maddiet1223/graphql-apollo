eb4baff8e712d954d6c353904a586f77ddb031b6
kiranmmu <mkiranala@gmail.com>
2023-11-19 23:08:01 -0600

    mutations

--- graphql-server-example/index.js
+++ graphql-server-example/index.js
@@ -44,6 +44,29 @@ const resolvers = {
         game(parent){
             return db.games.find((g)=>g.id === parent.game_id)
         },
+    },
+    Mutation:{
+        addGame(_,args){
+            let game = {
+                ...args.game,
+                id:Math.floor(Math.random()*10000).toString()  
+            }
+            db.games.push(game)
+            return game
+        },
+        deleteGame(_,args){
+            db.games = db.games.filter((g)=>g.id !== args.id)
+            return db.games
+        },   
+        updateGame(_,args){
+            db.games = db.games.map((g)=>{
+                if(g.id === args.id){
+                    return {...g,...args.edits}
+                }
+                return g
+            })
+            return db.games.find((g)=>g.id === args.id)
+        }
     }
     }       
 
--- graphql-server-example/schema.js
+++ graphql-server-example/schema.js
@@ -26,6 +26,19 @@ type Query{
   authors:[Author]
   author(id:ID!):Author
 }
+type Mutation{
+    addGame(game:AddGameInput!):Game
+    deleteGame(id:ID!):[Game]
+    updateGame(id:ID!,edits:EditGameInput!):Game
+}
+input AddGameInput{
+    title:String!,
+    platform:[String!]!
+}
+input EditGameInput{
+    title:String,
+    platform:[String!]
+}
 
 
 `
